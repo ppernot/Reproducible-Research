b647ffd66cd4270980ebc1dcf3975a9d7f9e2b8a
Pascal PERNOT <ppernot@users.noreply.github.com>
2017-04-12 15:00:49 +0200

    Delete build_figures.R

--- PUIF/Example_3/code/functions/build_figures.R
+++
@@ -1,9 +0,0 @@
-# Build all figures
-
-for (model_tag in list_meth) {
-  fit_tag = paste0("fit",model_tag)
-  load(paste0('simulation/',case,'/',fit_tag,'.rda'))
-  
-  plot_all(legend=list_legends[model_tag])
-  
-}
